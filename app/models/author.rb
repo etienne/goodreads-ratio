4753c52de58fe5b2408c9822b2a8888503f83686
Étienne Després <etienne@pochour.com>
2015-06-20 21:24:27 -0400

    Permet de récupérer plusieurs pages de reviews.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,16 +1,26 @@
 class Author < ActiveRecord::Base
-  def self.get_list_from_user_with_shelf(user_id, shelf)
-    uri = URI.parse "https://www.goodreads.com/review/list.xml?v=2&id=#{user_id}&shelf=#{shelf}&per_page=200&sort=author&key=#{ENV['GOODREADS_API_KEY']}"
-    xml = Timeout::timeout(10) { Net::HTTP.get(uri) }
+  def self.get_list_from_user_with_shelf(user_id, shelf, page = 1, authors = {})
+    per_page = 100
+    api_call_uri = "https://www.goodreads.com/review/list.xml?v=2&id=#{user_id}&shelf=#{shelf}&per_page=#{per_page}&page=#{page}&sort=author&key=#{ENV['GOODREADS_API_KEY']}"
+    uri = URI.parse api_call_uri
+    xml = Timeout::timeout(20) { Net::HTTP.get(uri) }
     tree = Nokogiri::XML(xml)
-    authors = {}
+    total_reviews = tree.xpath('//reviews').attribute('total').value
+    total_pages = (total_reviews.to_f / per_page).ceil
+
     tree.css('book > authors > author').each do |node|
       id = node.css('id').text
       name = node.css('name').text
       previous_count = authors[id].nil? ? 0 : authors[id].count
       authors[id] = { name: name, count: previous_count + 1 }
     end
-    authors
+    
+    if page >= total_pages
+      authors
+    else
+      sleep 1
+      get_list_from_user_with_shelf(user_id, shelf, page + 1, authors)
+    end
   end
   
   def self.get_gender(author_id)
@@ -22,9 +32,7 @@ class Author < ActiveRecord::Base
         cached: true
       }
     rescue ActiveRecord::RecordNotFound
-      if Author.count > 0
-        retry if Author.order(updated_at: :desc).limit(1).first.updated_at > (Time.now - 1.second)
-      end
+      sleep 1
       uri = URI.parse "https://www.goodreads.com/author/show.xml?id=#{author_id}&key=#{ENV['GOODREADS_API_KEY']}"
       xml = Timeout::timeout(10) { Net::HTTP.get(uri) }
       tree = Nokogiri::XML(xml)
